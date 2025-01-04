f62ad53d9e9ebc4375199eedb9d360c58398df22
Ahmed El-Shamy <ahmedelshamy14620@gmail.com>
2025-01-05 01:52:29 +0200

    refactor

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
-import 'package:marketi/core/utlis/app_router_config.dart';
-import 'package:marketi/core/widgets/custom_page_item.dart';
 import 'package:marketi/features/home/presentation/widgets/best_for_you_section.dart';
 import 'package:marketi/features/home/presentation/widgets/brands_section.dart';
 import 'package:marketi/features/home/presentation/widgets/buy_again_section.dart';
@@ -13,8 +10,8 @@ import 'package:marketi/features/home/presentation/widgets/special_offer_widget.
 import '../../../../core/utlis/app_assets.dart';
 import '../../../../core/utlis/app_colors.dart';
 import '../../../../core/widgets/custom_page_widget.dart';
-import '../../../../core/widgets/custom_search_text_field.dart';
 import '../../../../core/widgets/user_avatar_widget.dart';
+import '../widgets/home_search_section.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,27 +42,5 @@ class HomePage extends StatelessWidget {
         BuyAgainSection(),
       ],
     );
-  }
-}
-
-class HomeSearchField extends StatelessWidget {
-  const HomeSearchField({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomPageItem(
-      sliver: SliverToBoxAdapter(
-        child: SearchTextField(
-          readOnly: true,
-          onTap: () => _onTap(context),
-        ),
-      ),
-    );
-  }
-  
-  void _onTap(BuildContext context) {
-    GoRouter.of(context).push(AppRouterConfig.kSearchRoute);
   }
 }
