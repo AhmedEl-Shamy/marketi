9ba9f500dc25d9c69919657cf04104c182990041
Ahmed El-Shamy <ahmedelshamy14620@gmail.com>
2025-05-05 07:26:06 +0300

    finish popular products cubit

--- lib/features/home/presentation/controllers/popular_products_cubit/popular_products_cubit.dart
+++ lib/features/home/presentation/controllers/popular_products_cubit/popular_products_cubit.dart
@@ -1,10 +1,27 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketi/core/entities/product_entity.dart';
 import 'package:marketi/core/utlis/failure.dart';
-import 'package:meta/meta.dart';
+import 'package:marketi/features/home/domain/usecases/fetch_popular_products_usecase.dart';
 
 part 'popular_products_state.dart';
 
 class PopularProductsCubit extends Cubit<PopularProductsState> {
-  PopularProductsCubit() : super(PopularProductsInitial());
+  PopularProductsCubit({
+    required this.fetchPopularProductsUsecase,
+  }) : super(PopularProductsInitial());
+  final List<ProductEntity> products = [];
+  final FetchPopularProductsUsecase fetchPopularProductsUsecase;
+
+  Future<void> fetchPopularProducts({int limit = 10, int skip = 0}) async {
+    emit(PopularProductsLoading());
+    final result =
+        await fetchPopularProductsUsecase.call(limit: limit, skip: skip);
+    result.fold(
+      (failure) => emit(PopularProductsFailure(failure)),
+      (products) {
+        this.products.addAll(products);
+        emit(PopularProductsSuccess());
+      },
+    );
+  }
 }
--- lib/features/home/presentation/controllers/popular_products_cubit/popular_products_state.dart
+++ lib/features/home/presentation/controllers/popular_products_cubit/popular_products_state.dart
@@ -1,18 +1,15 @@
 part of 'popular_products_cubit.dart';
 
-@immutable
 sealed class PopularProductsState {}
 
 final class PopularProductsInitial extends PopularProductsState {}
-final class PopularProductsLoading extends PopularProductsState {}
-final class PopularProductsSuccess extends PopularProductsState {
-  final List<ProductEntity> products;
 
-  PopularProductsSuccess(this.products);
-}
+final class PopularProductsLoading extends PopularProductsState {}
+
+final class PopularProductsSuccess extends PopularProductsState {}
+
 final class PopularProductsFailure extends PopularProductsState {
   final Failure failure;
 
   PopularProductsFailure(this.failure);
 }
-
