8f7083c05eb8ae85f1edac6a3cf43f41072a7787
Ahmed El-Shamy <ahmedelshamy14620@gmail.com>
2025-05-05 07:28:14 +0300

    finish buy_again_cubit

--- lib/features/home/presentation/controllers/buy_agin_cubit/buy_again_cubit.dart
+++ lib/features/home/presentation/controllers/buy_agin_cubit/buy_again_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi/features/home/domain/usecases/fetch_buy_again.dart';
 
 import '../../../../../core/entities/product_entity.dart';
 import '../../../../../core/utlis/failure.dart';
@@ -6,5 +7,22 @@ import '../../../../../core/utlis/failure.dart';
 part 'buy_again_state.dart';
 
 class BuyAgainCubit extends Cubit<BuyAgainState> {
-  BuyAgainCubit() : super(BuyAgainInitial());
+  BuyAgainCubit({required this.fetchBuyAgainUsecase}) : super(BuyAgainInitial());
+  final List<ProductEntity> products = [];
+  final FetchBuyAgainUsecase fetchBuyAgainUsecase;
+
+  Future<void> fetchBuyAgain({int limit = 10, int skip = 0}) async {
+    emit(BuyAgainLoading());
+    final result = await fetchBuyAgainUsecase.call(
+      limit: limit,
+      skip: skip,
+    );
+    result.fold(
+      (failure) => emit(BuyAgainFailure(failure)),
+      (products) {
+        this.products.addAll(products);
+        emit(BuyAgainSuccess());
+      },
+    );
+  }
 }
--- lib/features/home/presentation/controllers/buy_agin_cubit/buy_again_state.dart
+++ lib/features/home/presentation/controllers/buy_agin_cubit/buy_again_state.dart
@@ -3,12 +3,11 @@ part of 'buy_again_cubit.dart';
 sealed class BuyAgainState {}
 
 final class BuyAgainInitial extends BuyAgainState {}
-final class BuyAgainLoading extends BuyAgainState {}
-final class BuyAgainSuccess extends BuyAgainState {
-  final List<ProductEntity> products;
 
-  BuyAgainSuccess(this.products);
-}
+final class BuyAgainLoading extends BuyAgainState {}
+
+final class BuyAgainSuccess extends BuyAgainState {}
+
 final class BuyAgainFailure extends BuyAgainState {
   final Failure failure;
 
